614cfeb2f8f57af90ba3b2e428d72d9bc0db60ca
smileyqp <468417415@qq.com>
2019-03-15 10:09:15 +0800

    polish

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,7 +9,8 @@ let router = express.Router();
 
 //判断请求信息是否为空的函数
 //此处data为前段请求所带过来的一串的数据
-const validateInput=(data)=>{
+//commonValidateInput此处是验证用户输入内容;包括是否为空,邮箱是否合法,密码和确认密码是否相同
+const commonValidateInput=(data)=>{
     console.log(data);
     let errors ={};//errors设置成空的对象
     if (validator.isEmpty(data.username)){
@@ -34,25 +35,34 @@ const validateInput=(data)=>{
 }
 
 
+const validateInput = (data,otherValidations)=>{
+    let {errors} = otherValidations(data);
+    return Promise;
+}
+
+
 router.post('/',(req,res)=>{
     // setTimeout(()=>{},500);
     //console.log(req.body);
-    const {errors,isValid} = validateInput(req.body);
-    if(isValid){
-        const {username,password,email} = req.body;
-        const password_digest = bcrypt.hashSync(password,10);//用bcrypt加密,哈希加密并且长度为10
+    const {errors,isValid} = validateInput(req.body,commonValidateInput).then(({errors,isvalid})=>{
+        if(isValid){
+            const {username,password,email} = req.body;
+            const password_digest = bcrypt.hashSync(password,10);//用bcrypt加密,哈希加密并且长度为10
+    
+            //保存进数据库中
+            User.forge({
+                username,password_digest,email
+            },{hasTimestamps:true}).save()//hasTimestamps为true是时间戳要存储起来
+            .then(user => res.json({success:true}))//成功返回一个user
+            .catch(err => res.status(500).json({errors:err}))//失败返回一个状态码500
+    
+            // res.json({success:true});
+        }else{
+            res.status(400).json(errors);//传给前台错误信息
+        }
 
-        //保存进数据库中
-        User.forge({
-            username,password_digest,email
-        },{hasTimestamps:true}).save()//hasTimestamps为true是时间戳要存储起来
-        .then(user => res.json({success:true}))//成功返回一个user
-        .catch(err => res.status(500).json({errors:err}))//失败返回一个状态码500
 
-        // res.json({success:true});
-    }else{
-        res.status(400).json(errors);//传给前台错误信息
-    }
+    });
 });
 
 
